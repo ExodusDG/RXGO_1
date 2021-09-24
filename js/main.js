fbf7ee57fe7b137c380654ec23761425c375b02f
Nikolay <68652897+SanistRay@users.noreply.github.com>
2021-09-25 00:47:06 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* DISCOUNT ADRESS  */
+
+$('.discount__adress').hover(
+    function() {
+        $(this).addClass('discount__text_hover')
+        $(this).find('span').css('display', 'block')
+    },
+    function() {
+        $('.discount__adress').find('span').css('display', 'none')
+        $('.discount__adress').removeClass('discount__text_hover')
+    })
+
+$('.discount__show_adress').click(function() {
+    $(this).closest('.discount__item').find('.discount__hidden').toggleClass('discount__hidden_show')
+})
+
+/* FAQ ITEM */
+
+$('.faq__item').click(function() {
+
+    if ($(this).find('.faq__item_answer').hasClass('faq__item_answer_open')) {
+        $(this).find('.faq__item_status').text('+')
+    } else {
+        $(this).find('.faq__item_status').text('-')
+    }
+
+    $(this).find('.faq__item_answer').toggleClass('faq__item_answer_open');
+    $(this).find('.faq__answer__text').toggleClass('faq__answer__text_visible')
+
+    $(this).find('.faq__item_number').toggleClass('faq__item_number_open')
+})
