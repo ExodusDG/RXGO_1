b12920b7caf843e7206b5b8add16e3532bc56f80
Nikolay <68652897+SanistRay@users.noreply.github.com>
2021-09-28 00:37:15 +0300

    23

--- js/main.js
+++ js/main.js
@@ -1,93 +1,43 @@
-    /* TOP BUTTON */
+/* DISCOUNT ADRESS  */
 
-    var progressValue = document.querySelector('.progress');
-    var RADIUS = 50;
-    var CIRCUMFERENCE = 2 * Math.PI * RADIUS;
-
-    function progress(value) {
-        var progress = value / 100;
-        var dashoffset = CIRCUMFERENCE * (1 - progress);
-        progressValue.style.strokeDashoffset = dashoffset;
-    }
-    progressValue.style.strokeDasharray = CIRCUMFERENCE;
-    var pageHeight = $(document).outerHeight(true);
-
-    $(window).on('scroll', function() {
-        var distanceScrolled = $(window).scrollTop();
-        var scrollPercent = (distanceScrolled * 100) / pageHeight;
-        progress(scrollPercent);
-    });
-
-    $.fn.isInViewport = function() {
-        var elementTop = $(this).offset().top;
-        var elementBottom = elementTop + $(this).outerHeight();
-
-        var viewportTop = $(window).scrollTop();
-        var viewportBottom = viewportTop + $(window).height();
-
-        return elementBottom > viewportTop && elementTop < viewportBottom;
-    };
-
-    $(window).on('resize scroll', function() {
-        if ($('.header').isInViewport()) {
-            $('.scroll_up').removeClass('scroll_up_visible');
-        } else {
-            $('.scroll_up').addClass('scroll_up_visible');
-        }
-    });
-
-    /* END */
-
-    /* SMOOTH SCROLL */
-
-    var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 600);
-        return false;
-    });
-
-    /* DISCOUNT ADRESS  */
-
-    $('.discount__adress').hover(
-        function() {
-            $(this).addClass('discount__text_hover')
-            $(this).find('span').css('display', 'block')
-        },
-        function() {
-            $('.discount__adress').find('span').css('display', 'none')
-            $('.discount__adress').removeClass('discount__text_hover')
-        })
-
-    $('.discount__show_adress').click(function() {
-        $(this).closest('.discount__item').find('.discount__hidden').toggleClass('discount__hidden_show')
+$('.discount__adress').hover(
+    function() {
+        $(this).addClass('discount__text_hover')
+        $(this).find('span').css('display', 'block')
+    },
+    function() {
+        $('.discount__adress').find('span').css('display', 'none')
+        $('.discount__adress').removeClass('discount__text_hover')
     })
 
-    /* FAQ ITEM */
+$('.discount__show_adress').click(function() {
+    $(this).closest('.discount__item').find('.discount__hidden').toggleClass('discount__hidden_show')
+})
 
-    $('.faq__item').click(function() {
+/* FAQ ITEM */
 
-        if ($(this).find('.faq__item_answer').hasClass('faq__item_answer_open')) {
-            $(this).find('.faq__item_status').text('+')
-        } else {
-            $(this).find('.faq__item_status').text('-')
-        }
+$('.faq__item').click(function() {
 
-        $(this).find('.faq__item_answer').toggleClass('faq__item_answer_open');
-        $(this).find('.faq__answer__text').toggleClass('faq__answer__text_visible')
-
-        $(this).find('.faq__item_number').toggleClass('faq__item_number_open')
-    })
-
-    var bodyWidth = $('body').width()
-
-    if (bodyWidth < 491) {
-        $('.info__full_logo').attr('src', 'image/logotypes__mb.png')
-
+    if ($(this).find('.faq__item_answer').hasClass('faq__item_answer_open')) {
+        $(this).find('.faq__item_status').text('+')
+    } else {
+        $(this).find('.faq__item_status').text('-')
     }
 
-    if (bodyWidth < 1024) {
+    $(this).find('.faq__item_answer').toggleClass('faq__item_answer_open');
+    $(this).find('.faq__answer__text').toggleClass('faq__answer__text_visible')
 
-        $('.coupon__price > img').attr('src', 'image/Component 16.png')
-    }
+    $(this).find('.faq__item_number').toggleClass('faq__item_number_open')
+})
+
+var bodyWidth = $('body').width()
+
+if (bodyWidth < 491) {
+    $('.info__full_logo').attr('src', 'image/logotypes__mb.png')
+
+}
+
+if (bodyWidth < 1024) {
+
+    $('.coupon__price > img').attr('src', 'image/Component 16.png')
+}
